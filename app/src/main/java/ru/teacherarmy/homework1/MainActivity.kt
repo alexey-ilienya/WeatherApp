c3b10805244ce6903592b195d7ba128fb3a3dbf7
Алексей Ильеня <ailienya1@gmail.com>
2026-01-16 11:25:21 +0300

    homework

--- app/src/main/java/ru/teacherarmy/homework1/MainActivity.kt
+++ app/src/main/java/ru/teacherarmy/homework1/MainActivity.kt
@@ -3,45 +3,27 @@ package ru.teacherarmy.homework1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.teacherarmy.homework1.ui.theme.Homework1Theme
+import dagger.hilt.android.AndroidEntryPoint
+import ru.teacherarmy.homework1.presentation.navigation.AppNavigation
+import ru.teacherarmy.homework1.ui.theme.WeatherAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            Homework1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            WeatherAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AppNavigation()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Homework1Theme {
-        Greeting("Android")
     }
 }
