00a0373790908863475dbee921872ff56d7672b5
Алексей Ильеня <ailienya1@gmail.com>
2026-02-02 13:06:54 +0300

    some lint rules added

---
+++ check/src/main/java/ru/teacherarmy/check/DirectColorInSource.kt
@@ -0,0 +1,59 @@
+package ru.teacherarmy.check
+
+import com.android.tools.lint.detector.api.Category.Companion.CORRECTNESS
+import com.android.tools.lint.detector.api.Severity.WARNING
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Implementation
+import com.android.tools.lint.detector.api.Issue
+import com.android.tools.lint.detector.api.JavaContext
+import com.android.tools.lint.detector.api.Scope
+import com.android.tools.lint.detector.api.SourceCodeScanner
+import com.android.tools.lint.detector.api.TextFormat
+import com.intellij.psi.PsiMethod
+import org.jetbrains.uast.UCallExpression
+
+class DirectColorInSource : Detector(), SourceCodeScanner {
+
+    override fun getApplicableMethodNames() = listOf("parseColor")
+
+    override fun visitMethodCall(context: JavaContext, node: UCallExpression, method: PsiMethod) {
+        if (context.evaluator.isMemberInClass(method, "android.graphics.Color")) {
+            //if it hits here it's because we have a direct color, no more checks needed
+            reportUsage(context, node)
+        }
+    }
+
+    private fun reportUsage(context: JavaContext, node: UCallExpression) {
+        context.report(
+            issue = ISSUE_DIRECT_COLOR_IN_SOURCE,
+            scope = node,
+            location = context.getCallLocation(
+                call = node,
+                includeReceiver = true,
+                includeArguments = true
+            ),
+            message = ISSUE_DIRECT_COLOR_IN_SOURCE.getExplanation(TextFormat.RAW)
+        )
+    }
+
+    companion object {
+        @JvmField
+        val ISSUE_DIRECT_COLOR_IN_SOURCE = Issue
+            .create(
+                id = "DirectColorInSource",
+                briefDescription = "Color method without theme reference",
+                explanation = """
+                    Always favor methods for retrieving colors with an attribute and a theme as parameters. Relying only in color resources won't make the code abstracted from its theme colors.
+                """.trimIndent(),
+                moreInfo = "https://material.io/develop/android/theming/color/",
+                category = CORRECTNESS,
+                priority = 7,
+                severity = WARNING,
+                androidSpecific = true,
+                implementation = Implementation(
+                    DirectColorInSource::class.java,
+                    Scope.JAVA_FILE_SCOPE
+                )
+            )
+    }
+}
---
+++ check/src/main/java/ru/teacherarmy/check/RetrofitReturnTypeDetector.kt
@@ -0,0 +1,91 @@
+package ru.teacherarmy.check
+
+import com.android.tools.lint.client.api.UElementHandler
+import com.android.tools.lint.detector.api.Detector
+import com.android.tools.lint.detector.api.Detector.UastScanner
+import com.android.tools.lint.detector.api.JavaContext
+import com.intellij.psi.PsiClassType
+import org.jetbrains.uast.UAnnotated
+import org.jetbrains.uast.UField
+import org.jetbrains.uast.UMethod
+import org.jetbrains.uast.UParameter
+import org.jetbrains.uast.getContainingUClass
+
+internal abstract class RetrofitReturnTypeDetector : Detector(), UastScanner {
+
+    override fun getApplicableUastTypes(): List<Class<UMethod>> = listOf(UMethod::class.java)
+
+    abstract class Visitor(private val context: JavaContext) : UElementHandler() {
+
+        private val listOfRetrofitAnnotations = listOf(
+            "retrofit2.http.DELETE",
+            "retrofit2.http.GET",
+            "retrofit2.http.POST",
+            "retrofit2.http.PUT",
+            "DELETE",
+            "GET",
+            "POST",
+            "PUT"
+        )
+
+        private val listOfRetrofitBodyAnnotations = listOf(
+            "retrofit2.http.Body",
+            "Body"
+        )
+
+        /**
+         * Return all field of return type of a retrofit interface method.
+         * Returned list is include recursive fields of complex classes and type information of genetic classes.
+         *
+         * Unit and Void return types are ignored.
+         *
+         * Static fields are ignored.
+         *
+         * @param node Method node to be check
+         * @return A list of fields of return type of method.
+         * Empty list if method doesn't belong to retrofit interface or method doesn't have valid return type.
+         */
+        fun findAllFieldsOf(node: UMethod): Set<UField> {
+            if (node.getContainingUClass()?.isInterface != true || !hasRetrofitAnnotation(node)) return emptySet()
+
+            val returnType = node.returnType
+            return when {
+                node.isSuspend() -> findAllInnerFields(node.parameters.last().type as PsiClassType)
+                returnType is PsiClassType && returnType.isResponseBody() -> emptySet()
+                returnType is PsiClassType && returnType.isNotUnitOrVoid() ->
+                    findAllInnerFields(returnType)
+
+                else -> emptySet()
+            }
+        }
+
+        fun findAllBodyParametersOf(node: UMethod): Set<UParameter> {
+            if (node.getContainingUClass()?.isInterface != true || !hasRetrofitAnnotation(node)) return emptySet()
+
+            return node.uastParameters.filter { hasBodyAnnotation(it) }.toSet()
+        }
+
+        private fun PsiClassType.isNotUnitOrVoid() =
+            !canonicalText.contains("Unit") && !canonicalText.contains("Void")
+
+        private fun PsiClassType.isResponseBody() = canonicalText.contains("ResponseBody")
+
+        private fun hasBodyAnnotation(parameter: UParameter): Boolean {
+            return context
+                .evaluator
+                .getAllAnnotations(parameter as UAnnotated, true)
+                .map { uAnnotation -> uAnnotation.qualifiedName }
+                .intersect(listOfRetrofitBodyAnnotations)
+                .isNotEmpty()
+        }
+
+        private fun hasRetrofitAnnotation(method: UMethod): Boolean {
+            return context
+                .evaluator
+                .getAllAnnotations(method as UAnnotated, true)
+                .map { uAnnotation -> uAnnotation.qualifiedName }
+                .intersect(listOfRetrofitAnnotations)
+                .isNotEmpty()
+        }
+    }
+}
